156efbc6b9b8f1220c9226e70035f6d0fd744d85
Enrico Sartori <sartori.enrico@gmail.com>
2009-04-22 21:40:31 +0200

    added utility for arff conversion

---
+++ utils/data2arff.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# Utility to convert dataset file in arff format
+
+import sys
+import os
+
+if len(sys.argv)!=3:
+        print 'Usage data2arff <input file> <output file>'
+        sys.exit()
+inp=open(sys.argv[1],'r')
+out=open(sys.argv[2],'w')
+attline=inp.readline()
+attrs=attline.split()
+out.write('% Xenon concentration dataset\n')
+out.write('@RELATION xenon\n\n')
+for a in attrs:
+        if a=='Index':
+                type='string'
+        else:
+                type='numeric'
+        out.write('@ATTRIBUTE '+a+' '+type+'\n')
+out.write('\n@DATA\n')
+line=inp.readline()
+while line!='':
+        data=line.split()
+        for d in data:
+                out.write(d+',')
+        out.write('\n')
+        line=inp.readline()
+inp.close()
+out.close()
+
+
+
+
