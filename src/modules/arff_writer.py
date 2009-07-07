a1877dcb6d0ba76a841b01d7ba6b98dc9bedec61
Enrico Sartori <enry@deathstar.(none)>
2009-07-07 15:19:00 +0200

    arff_writer commented

--- src/modules/arff_writer.py
+++ src/modules/arff_writer.py
@@ -1,4 +1,12 @@
+#!/usr/bin/env python
+
+
 class ArffWriter:
+    """
+    This class contains all the functions used for writing data in a .arff
+    file
+    """
+
     def __init__(self, filename, relation):
         self.file = filename
         self.relation = relation
@@ -6,6 +14,11 @@ class ArffWriter:
         self.buf_size = 100
 
     def write_headers(self, attrs):
+        """
+        This function writes the header of the file containing the
+        specificaion of the fields
+        """
+
         file = open(self.file,'w')
         file.write('@RELATION '+self.relation+'\n')
         for a in attrs:
@@ -17,12 +30,19 @@ class ArffWriter:
         file.close()
      
     def write_data(self,datapoint):
+        """
+        Writes the data in a temp buffer, when it's full the buffer is
+        stored on file
+        """
         self.buffer.append(datapoint)
         if len(self.buffer) >= self.buf_size:
             self.store_buffer()
         
 
     def store_buffer(self):
+        """
+        This function stores the content of the buffer in a file on disk
+        """
         file = open(self.file,'a')
         while len(self.buffer) > 0:
             dp = self.buffer.pop(0)
